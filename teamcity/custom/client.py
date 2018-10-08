bc20e4a0f71bf9e464c2cdfd86cf2adb41719e37
Aleksey Burov <allburov@gmail.com>
2018-10-08 15:14:07 +0700

    Add apis

--- teamcity/custom/client.py
+++ teamcity/custom/client.py
@@ -1,4 +1,4 @@
-from teamcity.api import AgentApi
+from teamcity.api import *
 from teamcity.api_client import ApiClient
 from teamcity.configuration import Configuration
 
@@ -16,64 +16,29 @@ class Teamcity(ApiClient):
 
         # Add "Managers" or APIs
         self.agents = AgentApi(self)
+        self.agent_pools = AgentPoolApi(self)
+        self.builds = BuildApi(self)
+        self.build_queues = BuildQueueApi(self)
+        self.build_types = BuildTypeApi(self)
+        self.changes = ChangeApi(self)
+        self.debug = DebugApi(self)
+        self.default = DefaultApi(self)
+        self.federation = FederationApi(self)
+        self.groups = GroupApi(self)
+        self.investigations = InvestigationApi(self)
+        self.problems = ProblemApi(self)
+        self.problem_occurrence = ProblemOccurrenceApi(self)
+        self.projects = ProjectApi(self)
+        self.server = ServerApi(self)
+        self.tests = TestApi(self)
+        self.test_occurrence = TestOccurrenceApi(self)
+        self.users = UserApi(self)
+        self.vcs_root = VcsRootApi(self)
+        self.vcs_root_instance = VcsRootInstanceApi(self)
 
-    def call_api(self, resource_path, method,
-                 path_params=None, query_params=None, header_params=None,
-                 body=None, post_params=None, files=None,
-                 response_type=None, auth_settings=None, async_req=None,
-                 _return_http_data_only=None, collection_formats=None,
-                 _preload_content=True, _request_timeout=None):
-        """Makes the HTTP request (synchronous) and returns deserialized data.
-
-        To make an async request, set the async_req parameter.
-
-        :param resource_path: Path to method endpoint.
-        :param method: Method to call.
-        :param path_params: Path parameters in the url.
-        :param query_params: Query parameters in the url.
-        :param header_params: Header parameters to be
-            placed in the request header.
-        :param body: Request body.
-        :param post_params dict: Request post form parameters,
-            for `application/x-www-form-urlencoded`, `multipart/form-data`.
-        :param auth_settings list: Auth Settings names for the request.
-        :param response: Response data type.
-        :param files dict: key -> filename, value -> filepath,
-            for `multipart/form-data`.
-        :param async_req bool: execute request asynchronously
-        :param _return_http_data_only: response data without head status code
-                                       and headers
-        :param collection_formats: dict of collection formats for path, query,
-            header, and post parameters.
-        :param _preload_content: if False, the urllib3.HTTPResponse object will
-                                 be returned without reading/decoding response
-                                 data. Default is True.
-        :param _request_timeout: timeout setting for this request. If one
-                                 number provided, it will be total request
-                                 timeout. It can also be a pair (tuple) of
-                                 (connection, read) timeouts.
-        :return:
-            If async_req parameter is True,
-            the request will be called asynchronously.
-            The method will return the request thread.
-            If parameter async_req is False or missing,
-            then the method will return the response directly.
+    def call_api(self, *args, **kwargs):
         """
-        auth_settings = ['Basic']
-        if not async_req:
-            return self._ApiClient__call_api(resource_path, method,
-                                   path_params, query_params, header_params,
-                                   body, post_params, files,
-                                   response_type, auth_settings,
-                                   _return_http_data_only, collection_formats,
-                                   _preload_content, _request_timeout)
-        else:
-            thread = self.pool.apply_async(self._ApiClient__call_api, (resource_path,
-                                                             method, path_params, query_params,
-                                                             header_params, body,
-                                                             post_params, files,
-                                                             response_type, auth_settings,
-                                                             _return_http_data_only,
-                                                             collection_formats,
-                                                             _preload_content, _request_timeout))
-        return thread
+        Quick hack for add Basic auth to swagger-codegen python
+        """
+        kwargs['auth_settings'] = ['Basic']
+        return super(Teamcity, self).call_api(*args, **kwargs)
