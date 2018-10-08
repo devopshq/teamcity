7b6bd9c40d8961acd678cd87a0a69d4610ca2a9f
Aleksey Burov <allburov@gmail.com>
2018-10-08 14:28:48 +0700

    model refactoring

--- generate.sh
+++ generate.sh
@@ -3,6 +3,7 @@ java -Xmx1024m -Xms256m -jar ./swagger-codegen-cli.jar generate \
     -l python \
     -c ./swagger/swagger-config.json \
     -t ./swagger/template/ \
+    -D models \
     -i ./swagger/swagger.json
 # I do not known why, but it is not working, and we have 'No description' in files...
 #    --additional-properties appDescription="Python Client for TeamCity REST API"
