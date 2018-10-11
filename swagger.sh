d83f205507d17c4f465500d4e10088cfa0e44091
Aleksey Burov <allburov@gmail.com>
2018-10-11 15:16:43 +0700

    swagger fix

--- swagger.sh
+++ swagger.sh
@@ -5,14 +5,13 @@ java -Xmx1024m -Xms256m -jar ./swagger-codegen-cli.jar generate \
     -t ./swagger/template/ \
     -D models -Dapis -Ddocs\
     -D supportingFiles=api_client.py \
-    -D supportingFiles=README.md \
     -i ./swagger/swagger.json
 ## I do not known why, but it is not working, and we have 'No description' in files...
 ##    --additional-properties appDescription="Python Client for TeamCity REST API"
 ##    -D appDescription="Python Client for TeamCity REST API"
 #
-#echo "" >> './dohq_teamcity/models/file.py'
-#echo "file = File" >> './dohq_teamcity/models/file.py'
+echo "" >> './dohq_teamcity/models/file.py'
+echo "file = File" >> './dohq_teamcity/models/file.py'
 mkdir ./docs-sphinx/swagger || echo "Swagger folder exist"
 mkdir ./docs-sphinx/swagger/api || echo "API folder exitst"
 mkdir ./docs-sphinx/swagger/models || echo "API folder exitst"
